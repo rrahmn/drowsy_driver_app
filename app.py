58fd2fab71db4ba2f49ca4a106e29dce212368b5
rrahmn <125041778+rrahmn@users.noreply.github.com>
2024-06-21 00:56:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def process_frame():
     return jsonify({'image': base64.b64encode(jpeg).decode('utf-8')})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(app.run(host='0.0.0.0', port=5000, ssl_context='adhoc'))
